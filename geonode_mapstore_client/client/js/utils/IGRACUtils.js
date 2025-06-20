1f69f0df4f4cebf09a18f3e7b8b7b9bf60d938a4
Irwan Fathurrahman <meomancer@gmail.com>
2025-06-20 12:43:39 +0700

    Fix layer selected on ggmn layers

--- geonode_mapstore_client/client/js/utils/IGRACUtils.js
+++ geonode_mapstore_client/client/js/utils/IGRACUtils.js
@@ -10,9 +10,6 @@ export function initIgracMapstore(mapConfig) {
         if (_layer.id && (_layer?.style?.includes("groundwater:Groundwater_Well") || _layer?.name?.includes("groundwater:"))) {
             let layerName = _layer.name;
             _layer.isIgracLayer = true;
-            setTimeout(function() {
-                window.MapStoreAPI.triggerAction({ type: 'LAYERS:SELECT_NODE', id: _layer.id, nodeType: 'layer', ctrlKey: false});
-            }, 500);
             layerName = 'groundwater:Groundwater_Well';
             let attributesUrl = `/api/layer/${layerName}/attributes`;
             axios.get(attributesUrl, {}).then((_response) => {
